c23b9469939e67d82b3547547ca2013a328f8d85
Trung Nghia Vu <nghiavtr@users.noreply.github.com>
2022-10-18 14:54:35 +0200

    Update runScasaDocker.sh

--- docker/runScasaDocker.sh
+++ docker/runScasaDocker.sh
@@ -45,13 +45,24 @@ printf "nthreads=${nthreads}\nwhitelist=${whitelist}\nindex=${index}\nindex_dir=
 path=`pwd`
 params_path="${path}/Scasa_temp"
 
-cmd="docker run -it"
-cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"
-cmd+=" -v ${INPUT}:/source/input"
-cmd+=" -v ${OUTPUT}:/source/output"
-cmd+=" -v ${index_dir}:/source/index_dir"
-cmd+=" -v ${params_path}:/source/params.sh"  
-cmd+=" nghiavtr/scasa:v1.0.1"
+
+if [ -v index_dir ]; then
+    cmd="docker run -it"
+    cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"
+    cmd+=" -v ${INPUT}:/source/input"
+    cmd+=" -v ${OUTPUT}:/source/output"
+    cmd+=" -v ${index_dir}:/source/index_dir"
+    cmd+=" -v ${params_path}:/source/params.sh"  
+    cmd+=" nghiavtr/scasa:v1.0.1"
+else
+    cmd="docker run -it"
+    cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"
+    cmd+=" -v ${INPUT}:/source/input"
+    cmd+=" -v ${OUTPUT}:/source/output"
+    cmd+=" -v ${params_path}:/source/params.sh"  
+    cmd+=" nghiavtr/scasa:v1.0.1"
+fi
+
 
 eval $cmd
 rm Scasa_temp
