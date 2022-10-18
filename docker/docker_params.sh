fb3ab94119d8f419a61de796fb0fffc389580ce4
Trung Nghia Vu <nghiavtr@users.noreply.github.com>
2022-10-18 14:41:39 +0200

    Update docker_params.sh

--- docker/docker_params.sh
+++ docker/docker_params.sh
@@ -4,7 +4,7 @@ INPUT="/path/to/Test_Dataset"
 OUTPUT="/path/to/Test_Dataset/ScasaOut"
 ref="/path/to/refMrna.fa.gz"
 index="YES" #when index="YES", scasa will index the reference fasta file and write in index_dir. This index_dir cam be reused for other run
-index_dir="/path/to/PreBuilt_REF_INDEX" #when index="NO", scasa will use directly the reference indexing in index_dir
+#index_dir="/path/to/PreBuilt_REF_INDEX" #when index="NO", scasa will use directly the reference indexing in index_dir
 nthreads=4
 tech="10xv3"
 whitelist="/path/to/Test_Dataset/Sample_01_Whitelist.txt"
