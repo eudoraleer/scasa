846cd276890273a389ba12b6d2bd6eb919f9155a
Trung Nghia Vu <nghiavtr@users.noreply.github.com>
2022-10-19 10:39:09 +0200

    Update runScasaDocker.sh

--- docker/runScasaDocker.sh
+++ docker/runScasaDocker.sh
@@ -24,7 +24,7 @@ case $key in
      *)
 
 esac
-
+shift
 done
 
 if [[ -n "$param" ]]; then
@@ -40,28 +40,67 @@ if [[ -n "$param" ]]; then
 fi
 
 
-printf "nthreads=${nthreads}\nwhitelist=${whitelist}\nindex=${index}\nindex_dir=${index_dir}" > Scasa_temp
+mycmd="printf '"
+if [[ -v nthreads ]]; then
+      mycmd+=" nthreads=${nthreads}\n"
+fi
+if [[ -v nwhitelist ]]; then
+      mycmd+=" whitelist=${whitelist}\n"
+fi
+if [[ -v index ]]; then
+      mycmd+=" index=${index}\n"
+fi
+if [[ -v index_dir ]]; then
+      mycmd+=" index_dir=${index_dir}\n"
+fi
+if [[ -v tech ]]; then
+      mycmd+=" tech=${tech}\n"
+fi
+if [[ -v cellthreshold ]]; then
+      mycmd+=" cellthreshold=${cellthreshold}\n"
+fi
+if [[ -v project ]]; then
+      mycmd+=" project=${project}\n"
+fi
+if [[ -v samplesheet ]]; then
+      mycmd+=" samplesheet=${samplesheet}\n"
+fi
+if [[ -v mapper ]]; then
+      mycmd+=" mapper=${mapper}\n"
+fi
+if [[ -v xmatrix ]]; then
+      mycmd+=" xmatrix=${xmatrix}\n"
+fi
+if [[ -v postalign_dir ]]; then
+      mycmd+=" postalign_dir=${postalign_dir}\n"
+fi
+if [[ -v createxmatrix ]]; then
+      mycmd+=" createxmatrix=${createxmatrix}\n"
+fi
+
+mycmd+="' > Scasa_temp"
+eval $mycmd
+
+
+
+#printf "nthreads=${nthreads}\nwhitelist=${whitelist}\nindex=${index}\nindex_dir=${index_dir}" > Scasa_temp
 
 path=`pwd`
 params_path="${path}/Scasa_temp"
 
 
-if [ -v index_dir ]; then
-    cmd="docker run -it"
-    cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"
-    cmd+=" -v ${INPUT}:/source/input"
-    cmd+=" -v ${OUTPUT}:/source/output"
+cmd="docker run -it"
+if [ "$index" = "NO" ]; then
+#    echo "USE INDEX DIR"
     cmd+=" -v ${index_dir}:/source/index_dir"
-    cmd+=" -v ${params_path}:/source/params.sh"  
-    cmd+=" nghiavtr/scasa:v1.0.1"
 else
-    cmd="docker run -it"
-    cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"
-    cmd+=" -v ${INPUT}:/source/input"
-    cmd+=" -v ${OUTPUT}:/source/output"
-    cmd+=" -v ${params_path}:/source/params.sh"  
-    cmd+=" nghiavtr/scasa:v1.0.1"
+#    echo "USE INDEX FASTA"
+    cmd+=" -v ${ref}:/source/referenceDB/refMrna.fa.gz"    
 fi
+cmd+=" -v ${INPUT}:/source/input"
+cmd+=" -v ${OUTPUT}:/source/output"
+cmd+=" -v ${params_path}:/source/params.sh"  
+cmd+=" nghiavtr/scasa:v1.0.1"
 
 
 eval $cmd
