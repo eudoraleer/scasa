0ec6f3a9e0e7982dce5554f09c53e580193a418f
Trung Nghia Vu <nghiavtr@users.noreply.github.com>
2023-09-12 09:34:58 +0200

    Speed up and optimise memory usage

--- scasa/SCRIPTS/Xmatrix_Gen/scasa_simulate_xmatrix_step1_v1.0.0.R
+++ scasa/SCRIPTS/Xmatrix_Gen/scasa_simulate_xmatrix_step1_v1.0.0.R
@@ -5,8 +5,12 @@
 #             Version V1.0.0
 #             Step: 1
 #             Author: Lu Pan, Trung Nghia Vu, Yudi Pawitan
-#             Last Update: 2021-04-07
+#             Last Update: 2023-09-09
 ##############################################################################
+### Nghia/12Sep2023: 
+# - speed up using parallel
+####
+
 packages <- c("GenomicFeatures","Biostrings")
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -36,8 +40,50 @@ readmat_input <- as.matrix(2*width(tx.export.fasta))
 readmat_input[readmat_input<1000] <- 1000
 
 data(cdnaf)
-simulate_experiment_countmat(xmatrix_fasta,
-                             readmat=readmat_input, outdir=output_dir,
+#simulate_experiment_countmat(xmatrix_fasta,
+#                             readmat=readmat_input, outdir=output_dir,
+#                             readlen = 91, paired = T, seed = 1000,
+#                             fraglen = 400,
+#                             fragsd = 100,
+#                             allow.nonnarrowing = FALSE,
+#                             distr = 'normal',
+#                             strand_specific = TRUE,
+#                             error_rate = 0 , bias="cdnaf")
+
+### simulate by chunks to avoid out of memory
+
+maxNum=length(tx.all.fasta)
+
+chunkSize=10000
+chunkNum=trunc((maxNum-1)/chunkSize)+1
+
+r1_fn=paste0(output_dir,"/sample_01_1.fasta")
+r2_fn=paste0(output_dir,"/sample_01_2.fasta")
+
+library(foreach)
+library(doParallel)
+core = detectCores()
+if (core > 1) core=round(core/2)
+registerDoParallel(cores=core)
+
+res=foreach(chunkInd=1:chunkNum) %dopar% {
+#for (chunkInd in 1:chunkNum){
+    library("polyester")
+    
+    chunkStart=(chunkInd-1)*chunkSize+1
+    chunkEnd=chunkInd*chunkSize
+    if (chunkEnd > maxNum) chunkEnd=maxNum
+
+    xmatrix_fasta_chunk=paste0(xmatrix_fasta,"_chunk_",chunkInd)
+    tx.export.fasta_chunk=tx.export.fasta[chunkStart:chunkEnd]
+    writeXStringSet(tx.export.fasta_chunk, xmatrix_fasta_chunk)
+    readmat_input_chunk <- as.matrix(2*width(tx.export.fasta_chunk))
+    readmat_input_chunk[readmat_input_chunk<1000] <- 1000
+
+    output_dir_chunk=paste0(output_dir,"/chunk",chunkInd,"/")
+
+    simulate_experiment_countmat(xmatrix_fasta_chunk,
+                             readmat=readmat_input_chunk, outdir=output_dir_chunk,
                              readlen = 91, paired = T, seed = 1000,
                              fraglen = 400,
                              fragsd = 100,
@@ -45,8 +91,26 @@ simulate_experiment_countmat(xmatrix_fasta,
                              distr = 'normal',
                              strand_specific = TRUE,
                              error_rate = 0 , bias="cdnaf")
+    return(NULL)
+}
 
-xmatrix_dir <- paste(output_dir,"SIMULATE_XMATRIX/", sep = "")
+for (chunkInd in 1:chunkNum){
+    output_dir_chunk=paste0(output_dir,"/chunk",chunkInd,"/")
+    if (chunkInd==1){
+        system(paste0("cp ",output_dir_chunk,"/*.fasta ",output_dir))
+    }else{
+        system(paste0("cat ",output_dir_chunk,"/sample_01_1.fasta >> ",r1_fn))
+        system(paste0("cat ",output_dir_chunk,"/sample_01_2.fasta >> ",r2_fn))
+    }
+    
+    system(paste0("rm -r ",output_dir_chunk))
+}
+
+system(paste0("rm ",xmatrix_fasta,"_chunk_*"))
+
+xmatrix_dir <- paste(output_dir,"/SIMULATE_XMATRIX/", sep = "")
+
+
 dir.create(xmatrix_dir, recursive = TRUE)
 system(paste("mv ", output_dir,"/sample_01_1.fasta ",xmatrix_dir,"/sample_01_2.fasta", sep = ""))
 system(paste("mv ", output_dir,"/sample_01_2.fasta ",xmatrix_dir,"/sample_01_1.fasta", sep = ""))
--- scasa/SCRIPTS/Xmatrix_Gen/scasa_simulate_xmatrix_step2_v1.0.0.R
+++ scasa/SCRIPTS/Xmatrix_Gen/scasa_simulate_xmatrix_step2_v1.0.0.R
@@ -5,35 +5,93 @@
 #             Version V1.0.0
 #             Step: 2
 #             Author: Lu Pan, Trung Nghia Vu, Yudi Pawitan
-#             Last Update: 2021-04-07
+#             Last Update: 2023-09-09
 ##############################################################################
+### Nghia/12Sep2023: 
+# - speed up using parallel
+# - optimize memory usage
+####
+
+
+library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 whitelist <- as.character(args[1])
 output_dir <- as.character(args[2])
 odd_sim_fasta <- as.character(args[3])
 
-white_list <- read.table(whitelist, header = F,stringsAsFactors=FALSE)
-white_list <- white_list[,1]
+#white_list <- read.table(whitelist, header = F,stringsAsFactors=FALSE)
+#white_list <- white_list[,1]
+white_list <- fread(whitelist, header = F)
+white_list <- white_list$V1
 
-txread_map=read.table(odd_sim_fasta, header = F,stringsAsFactors=FALSE)
-txread_map=txread_map[,1]
+
+#txread_map=read.table(odd_sim_fasta, header = F,stringsAsFactors=FALSE)
+#txread_map=txread_map[,1]
+txread_map=fread(odd_sim_fasta, header = F)
+txread_map=txread_map$V1
+
 txID.all=unique(txread_map)
 num_reads=length(txread_map)
+
+set.seed(2021)
+white_list.txID.all=sample(white_list,length(txID.all),replace = FALSE)
+white_list=NULL
+
+save(txID.all,white_list.txID.all, file=paste(output_dir,"/TxCellID_mapping.RData",sep = ""))
+
+
+
+matchID=match(txread_map,txID.all)
+txread_map=NULL
+txID.all=NULL
+
+current_white_list=white_list.txID.all[matchID]
+write.table(current_white_list, paste(output_dir,"/sample_01.white.list.txt",sep = ""), quote = F, row.names = F, col.names = F)
+white_list.txID.all=NULL
+current_white_list=NULL
+matchID=NULL
+
+
+print("Finished generating sample white list..")
+
 
 # UMI,12bp
 umi_size <- 12
 set.seed(2021)
-current_umi <- do.call(paste0, replicate(umi_size, sample(c("A","T","G","C"), num_reads, TRUE), FALSE))
-write.table(current_umi, paste(output_dir,"/sample_01.umi.txt",sep = ""), quote = F, row.names = F, col.names = F)
+#current_umi <- do.call(paste0, replicate(umi_size, sample(c("A","T","G","C"), num_reads, TRUE), FALSE))
+#write.table(current_umi, paste(output_dir,"/sample_01.umi.txt",sep = ""), quote = F, row.names = F, col.names = F)
+
+#do in chunks to reduce the memory
+maxNum=num_reads
+chunkSize=1000000
+chunkNum=trunc((maxNum-1)/chunkSize)+1
+
+library(foreach)
+library(doParallel)
+core = detectCores()
+if (core > 1) core=round(core/2)
+registerDoParallel(cores=core)
+
+res=foreach(chunkInd=1:chunkNum) %dopar% {
+#for (chunkInd in 1:chunkNum){
+	#cat(" ",chunkInd)
+    chunkStart=(chunkInd-1)*chunkSize+1
+    chunkEnd=chunkInd*chunkSize
+    if (chunkEnd > maxNum) chunkEnd=maxNum
+
+    num_reads_chunk=chunkEnd-chunkStart+1
+
+	current_umi <- do.call(paste0, replicate(umi_size, sample(c("A","T","G","C"), num_reads_chunk, TRUE), FALSE))
+	write.table(current_umi, paste0(output_dir,"/sample_01.umi_chunk_",chunkInd,".txt"), quote = F, row.names = F, col.names = F)
+	return(NULL)
+}
+
+for (chunkInd in 1:chunkNum){
+	#then combine them together
+	system(paste0("cat ",output_dir,"/sample_01.umi_chunk_",chunkInd,".txt >> ",output_dir,"/sample_01.umi.txt"))
+	system(paste0("rm ",output_dir,"/sample_01.umi_chunk_",chunkInd,".txt"))
+}
+
+
 print("Finished generating UMI barcode..")
 
-set.seed(2021)
-white_list.txID.all=sample(white_list,length(txID.all),replace = FALSE)
-save(txID.all,white_list.txID.all, file=paste(output_dir,"/TxCellID_mapping.RData",sep = ""))
-
-matchID=match(txread_map,txID.all)
-current_white_list=white_list.txID.all[matchID]
-
-write.table(current_white_list, paste(output_dir,"/sample_01.white.list.txt",sep = ""), quote = F, row.names = F, col.names = F)
-
-print("Finished generating sample white list..")
